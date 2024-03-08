4a3c2954d18778bc228503cb5e6d8ccc720567f0
Juddy2403 <119872018+Juddy2403@users.noreply.github.com>
2024-03-08 18:38:35 +0100

    Works but needs refactoring

--- Minigin/IMGUIComponent.cpp
+++ Minigin/IMGUIComponent.cpp
@@ -2,106 +2,132 @@
 
 #pragma warning( disable : 4244 )
 
-#include <chrono>
-#include <numeric>
-#include <set>
-
 GameEngine::IMGUIComponent::IMGUIComponent(GameObject* gameObj) : Component(gameObj)
-{
-	//CalculateArrayEx1();
-}
-
-void GameEngine::IMGUIComponent::CalculateArrayEx1(int nrOfSamples)
-{
-	constexpr int arrSize{ 67108864 };
-	int* arr = new int[arrSize];
-	std::vector<std::set<long>> timesRecorded(11);
-
-	for (size_t j = 0; j < nrOfSamples; j++)
-	{
-		int index{ -1 };
-		for (int stepsize = 1; stepsize <= 1024; stepsize *= 2)
-		{
-			auto start_time = std::chrono::high_resolution_clock::now();
-
-			for (int i = 0; i < arrSize; i += stepsize)
-			{
-				arr[i] *= 2;
-			}
-			auto end_time = std::chrono::high_resolution_clock::now();
-
-			const auto total = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
-			timesRecorded[++index].insert(total);
-		}
-	}
-	delete[] arr;
-	m_AvgTimes.clear();
-	for (auto& timesVec : timesRecorded)
-	{
-		float timeAverageVal = std::accumulate(++timesVec.begin(), --timesVec.end(), 0.f);
-		m_AvgTimes.push_back(timeAverageVal / ((nrOfSamples - 2) * 1000.0));
-	}
-}
+{}
 
 void GameEngine::IMGUIComponent::Render() 
 {
 	ImGui::NewFrame();
-	//ImGui::ShowDemoWindow();
-	ImGui::Begin("Exercise 1", nullptr, ImGuiWindowFlags_None);
 
-	static int nrOfSamples{ 10 };
-	ImGui::InputInt("# samples:", &nrOfSamples);
-	switch (m_PlotUpdateStage)
-	{
-	case PlotUpdateStage::noUpdate:
-		if (ImGui::Button("Trash the cache"))
-			m_PlotUpdateStage = PlotUpdateStage::clearButton;
-		break;
-	case PlotUpdateStage::clearButton:
-		m_PlotUpdateStage = PlotUpdateStage::showTextMsg;
-		break;
-	case PlotUpdateStage::showTextMsg:
-		ImGui::Text("Wait for it..");
-		m_PlotUpdateStage = PlotUpdateStage::updatePlot;
-		break;
-	case PlotUpdateStage::updatePlot:
-		UpdatePlot1(nrOfSamples);
-		m_PlotUpdateStage = PlotUpdateStage::noUpdate;
-		break;
-	}
+	//Manage exercise 1 graph
+	RenderExercise1();
 
-	ImGui::Plot("plot1", m_PlotConf);
+	//Manage exercise 2 graph
+	RenderExercise2();
 
-	ImGui::End();
 	ImGui::Render();
 	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
 
-void GameEngine::IMGUIComponent::UpdatePlot1(int nrOfSamples)
+void GameEngine::IMGUIComponent::RenderExercise1()
 {
-	constexpr size_t buf_size = 12;
-	static float x_data[buf_size] = { 1,2,4,8,16,32,64,128,256,512,1024 };
-
-	ImU32 color = ImColor(0, 255, 0);
-	static uint32_t selection_start = 0, selection_length = 0;
-
-	CalculateArrayEx1(nrOfSamples);
-	m_PlotConf.values.xs = x_data;
-	m_PlotConf.values.ys = m_AvgTimes.data();
-	m_PlotConf.values.count = buf_size;
-	m_PlotConf.values.color = color;
-	m_PlotConf.scale.min = 0;
-	m_PlotConf.scale.max = m_AvgTimes[0] + 1; // for y axis
-	m_PlotConf.tooltip.show = true;
-	m_PlotConf.grid_x.show = true;
-	m_PlotConf.grid_x.size = buf_size;
-	m_PlotConf.grid_x.subticks = buf_size;
-	m_PlotConf.grid_y.show = true;
-	m_PlotConf.grid_y.size = 0.5f;
-	m_PlotConf.grid_y.subticks = 5;
-	m_PlotConf.selection.show = true;
-	m_PlotConf.selection.start = &selection_start;
-	m_PlotConf.selection.length = &selection_length;
-	m_PlotConf.frame_size = ImVec2(buf_size * 25, 200);
-	m_PlotConf.line_thickness = 2.f;
+	ImGui::Begin("Exercise 1", nullptr, ImGuiWindowFlags_None);
+	static int nrOfSamples{ 10 };
+	ImGui::InputInt("# samples:", &nrOfSamples);
+	ManageIntPlotUpdateStages(nrOfSamples);
+	ImGui::Plot("Int plot", *m_IntPlotConf);
+	ImGui::End();
 }
+
+void GameEngine::IMGUIComponent::RenderExercise2()
+{
+	ImGui::Begin("Exercise 2", nullptr, ImGuiWindowFlags_None);
+
+	static int nrOfSamples{ 100 };
+	ImGui::InputInt("# samples:", &nrOfSamples);
+
+	ManageGameObjPlotUpdateStages(nrOfSamples);
+	ImGui::Plot("GameObj plot", *m_GameObjPlotConf);
+
+	ManageAltGameObjPlotUpdateStages(nrOfSamples);
+	ImGui::Plot("AltGameObj plot", *m_AltGameObjPlotConf);
+
+	if (!m_GameObjAvgTime.empty() && !m_AltGameObjAvgTime.empty())
+	{
+		ImGui::Text("Combined:");
+
+		static const float* y_data[] = { m_AltGameObjAvgTime.data(), m_GameObjAvgTime.data()};
+		static ImU32 colors[2] = { ImColor(255, 0, 0), ImColor(0, 0, 255) };
+
+		*m_CombinedPlotConf = *m_GameObjPlotConf;
+		m_CombinedPlotConf->values.color = 0;
+		m_CombinedPlotConf->values.colors = colors;
+		m_CombinedPlotConf->values.ys = nullptr;
+		m_CombinedPlotConf->values.ys_list = y_data;
+		m_CombinedPlotConf->values.ys_count = 2;
+		m_CombinedPlotConf->scale.max =std::max( *std::max_element(m_GameObjAvgTime.begin(), m_GameObjAvgTime.end()),
+			*std::max_element(m_AltGameObjAvgTime.begin(), m_AltGameObjAvgTime.end())) + 1; // for y axis
+		ImGui::Plot("Combined plot", *m_CombinedPlotConf);
+	}
+
+	ImGui::End();
+}
+
+void GameEngine::IMGUIComponent::ManageGameObjPlotUpdateStages(int nrOfSamples)
+{
+	switch (m_GameObjPlotUpdateStage)
+	{
+	case PlotUpdateStage::noUpdate:
+		if (ImGui::Button("Trash the cache with GameObject3D"))
+			m_GameObjPlotUpdateStage = PlotUpdateStage::clearButton;
+		break;
+	case PlotUpdateStage::clearButton:
+		m_GameObjPlotUpdateStage = PlotUpdateStage::showTextMsg;
+		break;
+	case PlotUpdateStage::showTextMsg:
+		ImGui::Text("Wait for it..");
+		m_GameObjPlotUpdateStage = PlotUpdateStage::updatePlot;
+		break;
+	case PlotUpdateStage::updatePlot:
+		UpdatePlot<GameObject3D>(*m_GameObjPlotConf, m_GameObjAvgTime,ImColor(0, 0, 255),nrOfSamples);
+		m_GameObjPlotUpdateStage = PlotUpdateStage::noUpdate;
+		break;
+	}
+}
+
+void GameEngine::IMGUIComponent::ManageIntPlotUpdateStages(int nrOfSamples)
+{
+	switch (m_IntPlotUpdateStage)
+	{
+	case PlotUpdateStage::noUpdate:
+		if (ImGui::Button("Trash the cache"))
+			m_IntPlotUpdateStage = PlotUpdateStage::clearButton;
+		break;
+	case PlotUpdateStage::clearButton:
+		m_IntPlotUpdateStage = PlotUpdateStage::showTextMsg;
+		break;
+	case PlotUpdateStage::showTextMsg:
+		ImGui::Text("Wait for it..");
+		m_IntPlotUpdateStage = PlotUpdateStage::updatePlot;
+		break;
+	case PlotUpdateStage::updatePlot:
+		//UpdateIntPlot(nrOfSamples);
+		UpdatePlot<int>(*m_IntPlotConf, m_IntAvgTime,ImColor(0, 255, 0), nrOfSamples);
+		m_IntPlotUpdateStage = PlotUpdateStage::noUpdate;
+		break;
+	}
+}
+
+void GameEngine::IMGUIComponent::ManageAltGameObjPlotUpdateStages(int nrOfSamples)
+{
+	switch (m_AltGameObjPlotUpdateStage)
+	{
+	case PlotUpdateStage::noUpdate:
+		if (ImGui::Button("Trash the cache with GameObject3DAlt"))
+			m_AltGameObjPlotUpdateStage = PlotUpdateStage::clearButton;
+		break;
+	case PlotUpdateStage::clearButton:
+		m_AltGameObjPlotUpdateStage = PlotUpdateStage::showTextMsg;
+		break;
+	case PlotUpdateStage::showTextMsg:
+		ImGui::Text("Wait for it..");
+		m_AltGameObjPlotUpdateStage = PlotUpdateStage::updatePlot;
+		break;
+	case PlotUpdateStage::updatePlot:
+		UpdatePlot<GameObject3DAlt>(*m_AltGameObjPlotConf,m_AltGameObjAvgTime, ImColor(255, 0, 0), nrOfSamples);
+		m_AltGameObjPlotUpdateStage = PlotUpdateStage::noUpdate;
+		break;
+	}
+}
+
+
--- Minigin/IMGUIComponent.h
+++ Minigin/IMGUIComponent.h
@@ -7,27 +7,135 @@
 #include <backends/imgui_impl_opengl3.h>
 #include <SDL_stdinc.h>
 #include <vector>
+#include <chrono>
+#include <numeric>
+#include <algorithm>
+#include <set>
 
-class vector;
+struct TransformM
+{
+	float matrix[16] = {
+		1,0,0,0,
+		0,1,0,0,
+		0,0,1,0,
+		0,0,0,1 };
+};
+
+class GameObject3D
+{
+public:
+	TransformM transform;
+	int ID;
+};
+class GameObject3DAlt
+{
+public:
+	TransformM* transform{};
+	int ID;
+
+	~GameObject3DAlt() { delete transform; }
+};
+
 namespace GameEngine {
-	
+
 	class IMGUIComponent : public Component
 	{
 	private:
-		void CalculateArrayEx1(int nrOfSamples = 10);
-		ImGui::PlotConfig m_PlotConf{};
-		std::vector<float> m_AvgTimes{};
+
+		std::unique_ptr<ImGui::PlotConfig> m_IntPlotConf{ std::make_unique< ImGui::PlotConfig>() };
+		std::unique_ptr<ImGui::PlotConfig> m_GameObjPlotConf{ std::make_unique< ImGui::PlotConfig>() };
+		std::unique_ptr<ImGui::PlotConfig> m_AltGameObjPlotConf{ std::make_unique< ImGui::PlotConfig>() };
+		std::unique_ptr<ImGui::PlotConfig> m_CombinedPlotConf{ std::make_unique< ImGui::PlotConfig>() };
+
+		std::vector<float> m_IntAvgTime;
+		std::vector<float> m_GameObjAvgTime;
+		std::vector<float> m_AltGameObjAvgTime;
+
 		enum class PlotUpdateStage {
 			noUpdate,
 			clearButton,
 			showTextMsg,
 			updatePlot
 		};
-		PlotUpdateStage m_PlotUpdateStage{ PlotUpdateStage::noUpdate };
-		void UpdatePlot1(int nrOfSamples);
+		PlotUpdateStage m_IntPlotUpdateStage{ PlotUpdateStage::noUpdate };
+		PlotUpdateStage m_GameObjPlotUpdateStage{ PlotUpdateStage::noUpdate };
+		PlotUpdateStage m_AltGameObjPlotUpdateStage{ PlotUpdateStage::noUpdate };
+
+		void ManageIntPlotUpdateStages(int nrOfSamples);
+		void ManageAltGameObjPlotUpdateStages(int nrOfSamples);
+		void RenderExercise1();
+
+		void ManageGameObjPlotUpdateStages(int nrOfSamples);
+
+		template<typename T>
+		void CalculateTimeAvg(int nrOfSamples, std::vector<float>& avgTimes)
+		{
+			constexpr int arrSize{ (std::is_same<T, int>::value) ? 67108864 : 10000000 };
+			T* arr = new T[arrSize]{};
+			std::vector<std::set<long>> timesRecorded(11);
+
+			for (size_t j = 0; j < nrOfSamples; j++)
+			{
+				int index{ -1 };
+				for (int stepsize = 1; stepsize <= 1024; stepsize *= 2)
+				{
+					auto start_time = std::chrono::high_resolution_clock::now();
+
+					for (int i = 0; i < arrSize; i += stepsize)
+					{
+						if constexpr (std::is_same<T, int>::value) {
+							arr[i] *= 2;
+						}
+						else if constexpr (std::is_same<T, GameObject3D>::value || std::is_same<T, GameObject3DAlt>::value) {
+							arr[i].ID *= 2;
+						}
+
+					}
+					auto end_time = std::chrono::high_resolution_clock::now();
+
+					const auto total = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
+					timesRecorded[++index].insert(total);
+				}
+
+			}
+			delete[] arr;
+			avgTimes.clear();
+			for (auto& timesVec : timesRecorded)
+			{
+				float timeAverageVal = std::accumulate(++timesVec.begin(), --timesVec.end(), 0.f);
+				avgTimes.push_back(timeAverageVal / (nrOfSamples - 2) / 100.f);
+			}
+		}
+
+		template<typename T>
+		void UpdatePlot(ImGui::PlotConfig& plotConf, std::vector<float>& avgTime, const ImU32& color, int nrOfSamples)
+		{
+			static constexpr size_t buf_size = 11;
+			static float x_data[buf_size] = { 1,2,4,8,16,32,64,128,256,512,1024 };
+
+			//ImU32 color = ImColor(0, 255, 0);
+			static uint32_t selection_start = 0, selection_length = 0;
+			CalculateTimeAvg<T>(nrOfSamples, avgTime);
+			plotConf.values.xs = x_data;
+			plotConf.values.ys = avgTime.data();
+			plotConf.values.count = buf_size;
+			plotConf.values.color = color;
+			plotConf.scale.min = 0;
+			plotConf.scale.max = *std::max_element(avgTime.begin(), avgTime.end()) + 1; // for y axis
+			plotConf.tooltip.show = true;
+			plotConf.grid_x.show = false;
+			plotConf.grid_y.show = true;
+			plotConf.selection.show = true;
+			plotConf.selection.start = &selection_start;
+			plotConf.selection.length = &selection_length;
+			plotConf.frame_size = ImVec2(250, 150);
+			plotConf.line_thickness = 2.f;
+		}
+
 	public:
 		explicit IMGUIComponent(GameObject* gameObj);
-
+		//void CalculateArray(int nrOfSamples, auto* arr, std::vector<float>& avgTimes);
 		virtual void Render() override;
+		void RenderExercise2();
 	};
 }
