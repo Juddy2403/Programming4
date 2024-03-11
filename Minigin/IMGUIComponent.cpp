19bc6ce1be4d37b97adf8678652d8536ed3350c6
Juddy2403 <119872018+Juddy2403@users.noreply.github.com>
2024-03-11 11:44:23 +0100

    small fixes 3

--- Minigin/IMGUIComponent.cpp
+++ Minigin/IMGUIComponent.cpp
@@ -17,10 +17,11 @@ void GameEngine::IMGUIComponent::Render()
 void GameEngine::IMGUIComponent::RenderExercise1()
 {
 	ImGui::Begin("Exercise 1", nullptr, ImGuiWindowFlags_None);
-	ImGui::InputInt("# samples:", &m_IntNrOfSamples);
+	static int nrOfSamples{ 10 };
+	ImGui::InputInt("# samples:", &nrOfSamples);
 	m_IntPlotUpdateInfo->buttonMsg = "Trash the cache";
 	m_IntPlotUpdateInfo->color = ImColor(0, 255, 0);
-	ManagePlotUpdateStages<int>(m_IntPlotUpdateInfo.get(), m_IntNrOfSamples);
+	ManagePlotUpdateStages<int>(m_IntPlotUpdateInfo.get(), nrOfSamples);
 	ImGui::Plot("Int plot", *m_IntPlotUpdateInfo->plotConfig);
 	ImGui::End();
 }
@@ -29,16 +30,17 @@ void GameEngine::IMGUIComponent::RenderExercise2()
 {
 	ImGui::Begin("Exercise 2", nullptr, ImGuiWindowFlags_None);
 
-	ImGui::InputInt("# samples:", &m_GameObjNrOfSamples);
+	static int nrOfSamples{ 100 };
+	ImGui::InputInt("# samples:", &nrOfSamples);
 
 	m_GameObjPlotUpdateInfo->buttonMsg = "Trash the cache with GameObject3D";
 	m_GameObjPlotUpdateInfo->color = ImColor(0, 0, 255);
-	ManagePlotUpdateStages<GameObject3D>(m_GameObjPlotUpdateInfo.get(), m_GameObjNrOfSamples);
+	ManagePlotUpdateStages<GameObject3D>(m_GameObjPlotUpdateInfo.get(), nrOfSamples);
 	ImGui::Plot("GameObj plot", *m_GameObjPlotUpdateInfo->plotConfig);
 
 	m_AltGameObjPlotUpdateInfo->buttonMsg = "Trash the cache with GameObject3DAlt";
 	m_AltGameObjPlotUpdateInfo->color = ImColor(255, 0, 0);
-	ManagePlotUpdateStages<GameObject3DAlt>(m_AltGameObjPlotUpdateInfo.get(), m_GameObjNrOfSamples);
+	ManagePlotUpdateStages<GameObject3DAlt>(m_AltGameObjPlotUpdateInfo.get(), nrOfSamples);
 	ImGui::Plot("AltGameObj plot", *m_AltGameObjPlotUpdateInfo->plotConfig);
 
 	if (!m_GameObjPlotUpdateInfo->avgTime.empty() && !m_AltGameObjPlotUpdateInfo->avgTime.empty())
