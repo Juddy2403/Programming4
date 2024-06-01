770fbab93d9bc129244c2f7fe392b9814f6350c2
Juddy2403 <119872018+Juddy2403@users.noreply.github.com>
2024-06-01 18:36:48 +0200

    fucking 2am code

--- GameProject/Game components/FormationComponent.h
+++ GameProject/Game components/FormationComponent.h
@@ -1,4 +1,5 @@
 ﻿#pragma once
+#include "Scene.h"
 #include "Components/Component.h"
 
 class FormationComponent final : public GameEngine::Component
@@ -6,10 +7,19 @@ class FormationComponent final : public GameEngine::Component
 public:
     explicit FormationComponent(GameEngine::GameObject* gameObj);
     void Update() override;
+    void LoadFormation(GameEngine::Scene* scene,std::vector<std::vector<std::unique_ptr<GameEngine::GameObject>>>&& formation,
+        const std::vector<GameEngine::IObserver*>& observers);
     ~FormationComponent() override = default;
     static float GetOffset();
     static void ToggleUpdate() { m_IsUpdating = !m_IsUpdating; }
 private:
+    std::vector<std::vector<std::unique_ptr<GameEngine::GameObject>>> m_Formation;
+    std::vector<GameEngine::GameObject*> m_CurrentlyLoadedEnemies;
+    std::vector<GameEngine::IObserver*> m_Observers;
+    GameEngine::Scene* m_Scene;
+    int m_CurrentStage;
+    int m_Stages;
+    bool m_LoadedStage{false};
     static bool m_IsUpdating;
     static bool m_DoesExist;
     static float m_Offset;
